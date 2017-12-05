0cc600442f600616285976eebaa9869154f74fb0
KS DIVYA <ksdivya16388@gmail.com>
2017-12-05 19:36:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var Twit = require('twit');
 var request = require('request');
 
 var T = new Twit({
- consumer_key:'HkjU2VMQFiJnojWlK2kungDeS',
- consumer_key_secret:'czHiKwSwyMk4jP2mXt0q31ZmLGi2qLDl075qlJ3khw47mYPEYo',
- acces_token:'937955235389095936-eXOaFD0ujcp6w6dgS0yzX3KIsOOdPI0',
- access_token_secret:'HmiNA4a5bKUWOnrNCEq8ej4aUmCecqTT3KVX3VRKxa68i'
+ consumer_key:'TOP SECRET KEY',
+ consumer_key_secret:'TOP SECRET KEY',
+ acces_token:'TOP SECRET KEY',
+ access_token_secret:'TOP SECRET KEY'
 })
 
 const url= "https://api.forismatic.com/api/1.0/?method=getQuote&key=692375&format=text&lang=en"
